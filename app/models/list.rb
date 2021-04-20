f1fccbead0cb87ab054218b250bd24a84aee9c91
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-04-20 13:21:29 +0200

    cloudinary and image upload now available

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,9 @@ class List < ApplicationRecord
   has_many :movies, through: :bookmarks
   has_many :reviews
 
+  # Cloudinary:
+  has_one_attached :photo
+
   # Validations:
   validates :name, uniqueness: true, presence: true
 end
