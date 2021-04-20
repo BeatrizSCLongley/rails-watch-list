dd6542b6d967ff960821af525acd0c86bbaed5ba
Beatriz Longley <beatrizsousacosta@gmail.com>
2021-04-20 12:41:24 +0200

    app done

---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  belongs_to :list
+
+  # Validations:
+  validates :comment, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+end
